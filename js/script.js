a1296022f8faad0d3c3bbd417636413febc46ecf
Adam Šáfr <adam.safr@seznam.cz>
2021-09-30 16:14:29 +0200

    rename

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Coolize.css", "Free Library"],
+        strings: ["Maestro.css", "Free Library"],
         typeSpeed: 100,
         backSpeed: 70,
         loop: true
